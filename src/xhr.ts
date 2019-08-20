f212f1ba09887c2e9d3a32380c32b7dd7ccb5b3b
Nice-Ming <244539171@qq.com>
2019-08-20 20:15:15 +0800

    feat: ts-axios 异常情况处理

--- src/xhr.ts
+++ src/xhr.ts
@@ -1,9 +1,10 @@
 import { AxiosRequestConfig, AxiosResponse, AxiosPromise } from './types'
 import { parseHeaders } from './helpers/headers'
+import { createError } from './helpers/error'
 
 export default function xhr(config: AxiosRequestConfig): AxiosPromise {
-  return new Promise(resolve => {
-    const { url, headers, method = 'get', data = null, responseType } = config
+  return new Promise((resolve, reject) => {
+    const { url, headers, method = 'get', data = null, responseType, timeout } = config
     const request = new XMLHttpRequest()
 
     if (responseType) {
@@ -12,8 +13,12 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
 
     request.open(method.toUpperCase(), url, true)
 
-    request.onreadystatechange = () => {
+    request.onreadystatechange = function handleLoad() {
       if (request.readyState !== 4) {
+        return
+      }
+
+      if (request.status === 0) {
         return
       }
 
@@ -30,7 +35,39 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
         request
       }
 
-      resolve(response)
+      handleResponse(response)
+    }
+
+    if (timeout) {
+      request.timeout = timeout
+    }
+
+    // 处理请求超时错误
+    request.ontimeout = function handleTimeout() {
+      reject(
+        createError(`Timeout of ${config.timeout} ms exceeded`, config, 'ECONNABORTED', request)
+      )
+    }
+
+    // 处理网络异常错误
+    request.onerror = function handleError() {
+      reject(createError('Network Error', config, null, request))
+    }
+
+    function handleResponse(response: AxiosResponse) {
+      if (response.status >= 200 && response.status < 300) {
+        resolve(response)
+      } else {
+        reject(
+          createError(
+            `Request failed with status code ${response.status}`,
+            config,
+            null,
+            request,
+            response
+          )
+        )
+      }
     }
 
     Object.keys(headers).forEach(name => {
