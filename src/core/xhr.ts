c097ff5bcce2b7f5b2f91d37362e5ecf0c8b6b89
Nice-Ming <244539171@qq.com>
2019-09-04 11:17:45 +0800

    feat: 支持上传和下载的进度监控

--- src/core/xhr.ts
+++ src/core/xhr.ts
@@ -2,10 +2,12 @@ import { AxiosRequestConfig, AxiosResponse, AxiosPromise } from '../types'
 import { parseHeaders } from '../helpers/headers'
 import { createError } from '../helpers/error'
 import { isURLSameOrigin } from '../helpers/url'
+import { isFormData } from '../helpers/util'
 import cookie from '../helpers/cookie'
 
 export default function xhr(config: AxiosRequestConfig): AxiosPromise {
 	return new Promise((resolve, reject) => {
+		const request = new XMLHttpRequest()
 		const {
 			url,
 			headers,
@@ -17,8 +19,9 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
 			withCredentials,
 			xsrfCookieName,
 			xsrfHeaderName,
+			onDownloadProgress,
+			onUploadProgress,
 		} = config
-		const request = new XMLHttpRequest()
 
 		function handleResponse(response: AxiosResponse) {
 			if (response.status >= 200 && response.status < 300) {
@@ -36,80 +39,111 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
 			}
 		}
 
-		if ((withCredentials || isURLSameOrigin(url!)) && xsrfCookieName) {
-			const xsrfValue = cookie.read(xsrfCookieName)
+		function configureRequest(): void {
+			if (responseType) {
+				request.responseType = responseType
+			}
 
-			if (xsrfValue && xsrfHeaderName) {
-				headers[xsrfHeaderName] = xsrfValue
+			if (timeout) {
+				request.timeout = timeout
+			}
+
+			if (withCredentials) {
+				request.withCredentials = true
 			}
 		}
 
-		if (responseType) {
-			request.responseType = responseType
+		function addEvents(): void {
+			request.onreadystatechange = function handleLoad() {
+				if (request.readyState !== 4) {
+					return
+				}
+
+				if (request.status === 0) {
+					return
+				}
+
+				const responseHeaders = parseHeaders(request.getAllResponseHeaders())
+				const responseData =
+					responseType && responseType !== 'text' ? request.response : request.responseText
+
+				const response: AxiosResponse = {
+					status: request.status,
+					statusText: request.statusText,
+					headers: responseHeaders,
+					data: responseData,
+					config,
+					request,
+				}
+
+				handleResponse(response)
+			}
+
+			// 处理请求超时错误
+			request.ontimeout = function handleTimeout() {
+				reject(
+					createError(`Timeout of ${config.timeout} ms exceeded`, config, 'ECONNABORTED', request)
+				)
+			}
+
+			// 处理网络异常错误
+			request.onerror = function handleError() {
+				reject(createError('Network Error', config, null, request))
+			}
+
+			if (onDownloadProgress) {
+				request.onprogress = onDownloadProgress
+			}
+
+			if (onUploadProgress) {
+				request.upload.onprogress = onUploadProgress
+			}
 		}
 
-		if (timeout) {
-			request.timeout = timeout
-		}
+		function processHeaders(): void {
+			if (isFormData(data)) {
+				delete headers['Content-Type']
+			}
 
-		if (withCredentials) {
-			request.withCredentials = true
-		}
+			if ((withCredentials || isURLSameOrigin(url!)) && xsrfCookieName) {
+				const xsrfValue = cookie.read(xsrfCookieName)
 
-		if (cancelToken) {
-			cancelToken.promise.then(reason => {
-				request.abort()
-				reject(reason)
+				if (xsrfValue && xsrfHeaderName) {
+					headers[xsrfHeaderName] = xsrfValue
+				}
+			}
+
+			Object.keys(headers).forEach(name => {
+				if (data === null && name.toLowerCase() === 'content-type') {
+					delete headers[name]
+				} else {
+					request.setRequestHeader(name, headers[name])
+				}
 			})
 		}
 
+		function processCancel(): void {
+			if (cancelToken) {
+				cancelToken.promise.then(reason => {
+					request.abort()
+					reject(reason)
+				})
+			}
+		}
 		// 运行时url是有值的 断言url不为空
 		request.open(method.toUpperCase(), url!, true)
 
-		request.onreadystatechange = function handleLoad() {
-			if (request.readyState !== 4) {
-				return
-			}
+		// 配置 request 对象
+		configureRequest()
 
-			if (request.status === 0) {
-				return
-			}
+		// 添加事件处理函数
+		addEvents()
 
-			const responseHeaders = parseHeaders(request.getAllResponseHeaders())
-			const responseData =
-				responseType && responseType !== 'text' ? request.response : request.responseText
+		// 处理请求 headers
+		processHeaders()
 
-			const response: AxiosResponse = {
-				status: request.status,
-				statusText: request.statusText,
-				headers: responseHeaders,
-				data: responseData,
-				config,
-				request,
-			}
-
-			handleResponse(response)
-		}
-
-		// 处理请求超时错误
-		request.ontimeout = function handleTimeout() {
-			reject(
-				createError(`Timeout of ${config.timeout} ms exceeded`, config, 'ECONNABORTED', request)
-			)
-		}
-
-		// 处理网络异常错误
-		request.onerror = function handleError() {
-			reject(createError('Network Error', config, null, request))
-		}
-
-		Object.keys(headers).forEach(name => {
-			if (data === null && name.toLowerCase() === 'content-type') {
-				delete headers[name]
-			} else {
-				request.setRequestHeader(name, headers[name])
-			}
-		})
+		// 处理请求取消逻辑
+		processCancel()
 
 		request.send(data)
 	})
